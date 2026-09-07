7af4660a65b87e69e686bef2a325d268e94325f5
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2026-09-07 12:35:05 -0400

    refactor: Sonar cleanup, part 6 (ServletFlag)
    ServletFlag is a global as its docs said. Therefore,
    it should not be instantiable. The tests needed to
    be restructured since they were creating an instance.
    
    Now the test resets the AtomicBoolean isRunningFromServlet
    field before each test, and exercises the functionality
    of ServletFlag directly.

--- src/test/java/spark/globalstate/ServletFlagTest.java
+++ src/test/java/spark/globalstate/ServletFlagTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.kiwiproject.reflect.KiwiReflection;
+import org.kiwiproject.reflect.RuntimeReflectionException;
 
 import java.lang.reflect.Field;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -13,40 +14,32 @@ class ServletFlagTest {
 
     @BeforeEach
     void setUp() {
-        KiwiReflection.setFieldValue(null, isRunningFromServletField(), new AtomicBoolean(false));
-    }
-
-    @Test
-    void testRunFromServlet_whenDefault() {
-
-        AtomicBoolean isRunningFromServlet = KiwiReflection.getTypedFieldValue(null, isRunningFromServletField(), AtomicBoolean.class);
-        assertThat(isRunningFromServlet.get()).isFalse();
-    }
-
-    @Test
-    void testRunFromServlet_whenExecuted() {
-
-        ServletFlag.runFromServlet();
-        AtomicBoolean isRunningFromServlet = KiwiReflection.getTypedFieldValue(null, isRunningFromServletField(), AtomicBoolean.class);
-
-        assertThat(isRunningFromServlet.get()).isTrue();
+        resetIsRunningFromServlet();
     }
 
     @Test
     void testIsRunningFromServlet_whenDefault() {
-
         assertThat(ServletFlag.isRunningFromServlet()).isFalse();
-
     }
 
     @Test
     void testIsRunningFromServlet_whenRunningFromServlet() {
-
         ServletFlag.runFromServlet();
         assertThat(ServletFlag.isRunningFromServlet()).isTrue();
     }
 
-    private static Field isRunningFromServletField() {
-        return KiwiReflection.findField(new ServletFlag(), "isRunningFromServlet");
+    private static void resetIsRunningFromServlet() {
+        var field = getIsRunningFromServletField();
+        field.setAccessible(true);
+        var fieldValue = KiwiReflection.getTypedFieldValue(null, field, AtomicBoolean.class);
+        fieldValue.set(false);
+    }
+
+    private static Field getIsRunningFromServletField() {
+        try {
+            return ServletFlag.class.getDeclaredField("isRunningFromServlet");
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeReflectionException(e);
+        }
     }
 }
