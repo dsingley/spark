1c6a2fc3e5f3ca4e046c8565c87bed5651991984
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2026-09-11 22:21:02 +0000

    fix: replace usage of deprecated URL constructor

--- src/main/java/spark/utils/ResourceUtils.java
+++ src/main/java/spark/utils/ResourceUtils.java
@@ -103,7 +103,7 @@ public abstract class ResourceUtils {
             return true;
         }
         try {
-            new URL(resourceLocation);
+            URI.create(resourceLocation).toURL();
             return true;
         } catch (MalformedURLException ex) {
             return false;
@@ -177,14 +177,14 @@ public abstract class ResourceUtils {
         if (separatorIndex != -1) {
             var jarFile = urlFile.substring(0, separatorIndex);
             try {
-                return new URL(jarFile);
+                return URI.create(jarFile).toURL();
             } catch (MalformedURLException ex) {
                 // Probably no protocol in original jar URL, like "jar:C:/mypath/myjar.jar".
                 // This usually indicates that the jar file resides in the file system.
                 if (!jarFile.startsWith("/")) {
                     jarFile = "/" + jarFile;
                 }
-                return new URL(FILE_URL_PREFIX + jarFile);
+                return URI.create(FILE_URL_PREFIX + jarFile).toURL();
             }
         } else {
             return jarUrl;
